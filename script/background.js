f2731f51bf80de586471863bcf65a5cedee51a48
黄俊辉 <huangjunhui19@crc.com.hk>
2019-08-16 18:05:34 +0800

    init

---
+++ script/background.js
@@ -0,0 +1,63 @@
+
+// 记录被阻止的广告
+const replaceList = {}
+
+// 需要阻止的广告规则：正则
+const matches = [
+  /googlesyndication.com/,
+  /linuxprobe.com/,
+  /hm.baidu.com/,
+  /\/\/t\d+.baidu.com/,
+  /cpro.baidustatic.com/,
+  /mediav.com/,
+  /cdn.v2ex.com\/assets\/sidebar/,
+  /static.oschina.net\/uploads\/cooperation/,
+  /rabc2.iteye.com/,
+  /adaccount.csdn.net/,
+]
+
+// 监听网络请求，阻止的广告加载
+chrome.webRequest.onBeforeRequest.addListener(function (d) {
+  // console.log(d)
+  const tabid = String(d.tabId)
+
+  replaceList[tabid] = replaceList[tabid] ? replaceList[tabid] : []
+  var url = d.url;
+  var isCancel = false
+
+  matches.map(m => {
+    if (m.test(url)) {
+      if (replaceList[tabid].indexOf(url) === -1) {
+        replaceList[tabid].push(url)
+      }
+      isCancel = true
+      setIcon(tabid)
+    }
+  })
+
+  return {
+    cancel: isCancel
+  }
+},
+  { urls: ["<all_urls>"] }, // 在所有网页执行
+  ["blocking"] // 阻止源请求
+)
+
+console.log(replaceList)
+
+function getReplaceList() {
+  return replaceList
+}
+
+// 如果有加速资源则点亮图标
+function setIcon(tabid) {
+  chrome.browserAction.setIcon({
+    tabId: parseInt(tabid),
+    path: {
+      "16": "icons/128.png",
+      "48": "icons/128.png",
+      "128": "icons/128.png"
+    }
+  })
+}
+
---
+++ script/popup.js
@@ -0,0 +1,25 @@
+// 把替换过的资源显示出来
+const CBG = chrome.extension.getBackgroundPage()
+const repalceListEle = document.querySelector("#repalceList")
+const adsCount = document.querySelector("#adsCount")
+const replaceList = CBG.getReplaceList()
+let tabid = ""
+let list = ""
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  // console.log(tabs)
+  tabid = String(tabs[0].id)
+
+  adsCount.innerHTML = replaceList[tabid].length
+
+  if (replaceList[tabid] && replaceList[tabid].length > 0) {
+    replaceList[tabid].map((item) => {
+      list += `<li>${item}</li>`
+    })
+  } else {
+    list = "<li>没有找到广告</li>"
+  }
+
+  repalceListEle.innerHTML = list
+
+});
